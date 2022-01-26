5aa93f7f57f975afeec5d120a6af3e82c3bbe1a3
= <=>
2022-01-26 05:05:29 +0300

    app commit

---
+++ app/src/main/java/com/abdullahdurna/contrieskotlin/view/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.abdullahdurna.contrieskotlin.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.abdullahdurna.contrieskotlin.R
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
+
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Banner Ad ID
+        //ca-app-pub-7014502045756157/6735939367
+        //Test Banner Ad ID
+        //ca-app-pub-3940256099942544/6300978111
+
+
+    }
+}
